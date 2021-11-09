241c60fbf1d00498b7eb4727ad8be42fdc913dac
Evan Burton <butonevan1@gmaail.com>
2021-11-09 14:29:07 -0700

    moved json file to seeds, added routes

--- server.js
+++ server.js
@@ -7,13 +7,54 @@ const app = express();
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(
-  process.env.MONGODB_URL,
-  { useUnifiedTopology: true, useNewUrlParser: true },
-  )
-  .then(function(){
-    console.log('Connected to DB...')
-  })
-  .catch(function(err){
-    console.log(err)
-  });
+
+const Place = require('./models/Places')
+console.log(Place);
+
+// List entry route
+app.get('/api/travel', async (request, response) => {
+
+  const places = await Places.find()
+
+  response.send(places)
+
+})
+
+// Item route
+app.get('/api/travel/:title', async (request, response) => {
+
+  try {
+    const places = await Places.findOne({title: request.params.title})
+    if (!places) {
+      throw new Error()
+    }
+    response.send(places)
+  } catch(err) {
+    response.send({error: 'Places Not Found'})
+  }
+  
+})
+// Handle 404 errors with middleware
+app.use(function(request, response) {
+
+  // If path starts with `/api`, send JSON 404
+  if (request.url.startsWith('/api')) {
+
+    response.status(404)
+    response.send({error: 'File Noooot Found'})
+
+  } else {
+  
+    // else send HTML 404
+    response.status(404)
+    response.send('<h1>404: File Not Found</h1>')
+
+  }
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
