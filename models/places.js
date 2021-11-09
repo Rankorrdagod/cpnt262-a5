28996dc2f473ca0470ac93b40e12e43fc2a8345e
Evan Burton <butonevan1@gmaail.com>
2021-11-09 11:56:15 -0700

    added db.js and places.js

---
+++ models/places.js
@@ -0,0 +1,28 @@
+const mongoose = require('./db')
+
+/*****************/
+/* Define Schema */
+/*****************/
+
+const placeSchema = new mongoose.Schema({
+  id: Number,
+  title: {
+    type: String,
+    unique: true
+  },
+  description: String,
+  width: String,
+  height: String,
+  pathURL: String,
+  linkURL: String,
+  credit: String,
+  creditURL: String,
+  
+
+})
+
+/*****************/
+/* Compile Model */
+/*****************/
+
+module.exports = mongoose.model('Place', placeSchema)
