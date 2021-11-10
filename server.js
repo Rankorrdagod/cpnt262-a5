7e35493f709ee9104170ee696f100ae175455a0f
Evan Burton <butonevan1@gmaail.com>
2021-11-09 17:10:26 -0700

    bug squashing

--- server.js
+++ server.js
@@ -7,48 +7,26 @@ const app = express();
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
+const api = require('./routes/api')
+app.use('/api', api)
 
-const Place = require('../models/Places')
-console.log(Place);
 
-// List entry route
-app.get('/api/travel', async (request, response) => {
 
-  const places = await Places.find()
-
-  response.send(places)
-
-})
-
-// Item route
-app.get('/api/travel/:title', async (request, response) => {
-
-  try {
-    const places = await Places.findOne({title: request.params.title})
-    if (!places) {
-      throw new Error()
-    }
-    response.send(places)
-  } catch(err) {
-    response.send({error: 'Places Not Found'})
-  }
-  
-})
 // Handle 404 errors with middleware
-app.use(function(request, response) {
+app.use((req, res) => {
 
   // If path starts with `/api`, send JSON 404
-  if (request.url.startsWith('/api')) {
+  if (req.url.startsWith('/api')) {
 
-    response.status(404)
-    response.send({error: 'File Noooot Found'})
+    res.status(404)
+    res.send({error: 'File Not Found'})
 
   } else {
   
     // else send HTML 404
-    response.status(404)
-    response.send('<h1>404: File Not Found</h1>')
-
+    res.status(404)
+    res.redirect('404.html')
+    //res.sendFile(__dirname + '/public/404.html')
   }
 });
 
@@ -58,3 +36,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
   console.log(`Listening on port ${PORT}`);
 });
+
+   
