f6c33efaa43bdf38364d65b877210d899ffea53d
Evan Burton <butonevan1@gmaail.com>
2021-11-10 09:30:51 -0700

    push for heroku

--- server.js
+++ server.js
@@ -14,23 +14,20 @@ const mongoose = require('./models/db')
 
 // Handle 404 errors with middleware
 app.use((req, res) => {
+  try {
 
-  // If path starts with `/api`, send JSON 404
-  if (req.url.startsWith('/api')) {
-
-    res.status(404)
-    res.send({error: 'File Not Found'})
-
-  } else {
-  
-    // else send HTML 404
-    res.status(404)
-    res.redirect('/404.html')
-    //res.sendFile(__dirname + '/public/404.html')
+    if (req.url.startsWith('/api')) {
+      res.status(404).send({ error: '404 Not Found' })
+    } else {
+     
+      res.status(404).redirect('/404.html') 
+    }
+  } catch {
+    res.status(404).send({ error: '404 Not Found' }) 
   }
 });
 
-// Start server
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
